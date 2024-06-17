01adf08d3a01d8a49c93c4a28f50e9435432813a
Vh3z <marco.vecchio0013@gmail.com>
2024-06-17 17:36:43 +0200

    BackEnd: Elimina Prenotazione e Aggiorna Prezzi

--- codice/ParkCharge_Backend/src/main/java/DataBase/DbStorico.java
+++ codice/ParkCharge_Backend/src/main/java/DataBase/DbStorico.java
@@ -48,7 +48,7 @@ public class DbStorico {
         return list;
     }
 
-    public void update(String comandoSql)
+    public boolean update(String comandoSql)
     {
         Connection conn;
         Statement stmt;
@@ -59,13 +59,16 @@ public class DbStorico {
         {
             conn = DriverManager.getConnection("jdbc:sqlite:dati/DbStorico");
             stmt = conn.createStatement();
-            stmt.executeUpdate(comandoSql);
+            if(stmt.executeUpdate(comandoSql) == 0)
+                return false;
             stmt.close();
             conn.close();
+            return true;
         }
         catch (SQLException e)
         {
             e.printStackTrace();
+            return false;
         }
     }
 }
