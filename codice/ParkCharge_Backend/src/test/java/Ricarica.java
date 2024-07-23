2c7c3d346a14351de10d812961983becf83bfa85
LMetal <leogall2002@gmail.com>
2024-07-23 14:54:47 +0200

    test richiedi ricarica, interrompi ricarica

---
+++ codice/ParkCharge_Backend/src/test/java/Ricarica.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class Ricarica {
+    GestoreRicariche gestoreRicariche = new GestoreRicariche();
+    @Test
+    public void richiediInterrompiRicarica(){
+        var ricaricheList = gestoreRicariche.getRicariche();
+        assertFalse(gestoreRicariche.getRicariche().stream().anyMatch(p -> p.getPrenotazione() == 100));
+        assertEquals(10, ricaricheList.size());
+
+        gestoreRicariche.addRicarica(20, 100);
+        ricaricheList = gestoreRicariche.getRicariche();
+        assertEquals(11, ricaricheList.size());
+        assertTrue(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 100));
+
+        assertTrue(gestoreRicariche.stopRicarica(100));
+        ricaricheList = gestoreRicariche.getRicariche();
+        assertEquals(10, ricaricheList.size());
+        assertFalse(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 100));
+
+
+
+        /*ricaricheList = gestoreRicariche.getRicariche();
+        assertFalse(gestoreRicariche.getRicariche().stream().anyMatch(p -> p.getPrenotazione() == 100));
+        assertEquals(10, ricaricheList.size());
+
+        gestoreRicariche.addRicarica(20, 100);
+        gestoreRicariche.addRicarica(20, 101);
+        ricaricheList = gestoreRicariche.getRicariche();
+        assertEquals(12, ricaricheList.size());
+        assertTrue(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 100));
+        assertTrue(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 101));
+
+        gestoreRicariche.stopRicarica(100);
+        gestoreRicariche.stopRicarica(101);
+        ricaricheList = gestoreRicariche.getRicariche();
+        assertEquals(10, ricaricheList.size());
+        assertFalse(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 100));
+        assertFalse(ricaricheList.stream().anyMatch(p -> p.getPrenotazione() == 101));*/
+    }
+}
